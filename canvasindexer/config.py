44c5558cb972f5a1cf5f1c43d51ed51c3c013ef1
Tarek Saier <tareksaier@gmail.com>
2018-11-03 19:22:00 +0100

    improve handling of cfg file and default settings
    - even though defaults were in place, Canvas Indexer
      would complain when there's no config.ini file
    - having the config.ini as part of the repository
      wasn't nice. now there's config.ini.example

--- canvasindexer/config.py
+++ canvasindexer/config.py
@@ -10,10 +10,10 @@ class Cfg():
 
     def __init__(self, path='config.ini'):
         cp = configparser.ConfigParser()
-        if not os.path.exists(path):
-            print('Config file "{}" not found.\nExiting.'.format(path))
-            sys.exit(1)
-        cp.read(path)
+        if os.path.exists(path):
+            cp.read(path)
+        else:
+            print('No config file "{}". Using defaults.'.format(path))
         fail, cfg = self._parse_config(cp)
         if fail:
             print(fail)
